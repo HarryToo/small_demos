b6c31f09c86962eb07d53b51793ee6a89d9ce0d9
duhang <840261937@qq.com>
2018-07-23 17:57:52 +0800

    微调

--- memory_game/js/memory_game.js
+++ memory_game/js/memory_game.js
@@ -1,9 +1,11 @@
 var imgArr = ['images/pidan.jpg', 'images/heiniu.jpg', 'images/xiaoqiang.jpg', 'images/pangdun.jpg', 'images/congtou.jpg', 'images/baobao.jpg'];
 var currCard = null, eliminateNum = 0;
 
+// 初始化
 function init() {
     var html = '';
     for (var i = 0; i < 2; i++) {
+        // 打乱牌序
         imgArr.sort(function () {
             return (0.5 - Math.random());
         });
@@ -11,30 +13,38 @@ function init() {
             html += '<li class="animated"><div class="animated"></div><img src="' + imgArr[j] + '" class="animated"></li>';
         }
     }
+    // 发牌
     $('ul').html(html);
     handle();
 }
 
 init();
 
+// 点击事件处理
 function handle() {
     $('ul').on('click', 'li div', function () {
         var $this = $(this).parent();
         openCard($this);
         if (currCard === null) {
+            // 记录下翻到的第一张牌
             currCard = $this;
         } else {
+            // 翻第二张牌
             if (currCard.find('img').attr('src') === $this.find('img').attr('src')) {
+                // 若匹配为一对，则消除这两张牌
                 eliminateCard($this, currCard);
             } else {
+                // 如不匹配，则重新盖上这两张牌
                 closeCard(currCard);
                 closeCard($this);
             }
+            // 无论如何，翻开二张后重置之前翻到的牌
             currCard = null;
         }
     });
 }
 
+// 翻开
 function openCard(item) {
     item.find('div').removeClass('flipInY').addClass('flipOutY');
     setTimeout(function () {
@@ -42,6 +52,7 @@ function openCard(item) {
     }, 250);
 }
 
+// 盖上
 function closeCard(item) {
     setTimeout(function () {
         item.find('img').removeClass('flipInY').addClass('flipOutY');
@@ -51,7 +62,9 @@ function closeCard(item) {
     }, 400);
 }
 
+// 消除
 function eliminateCard(item1, item2) {
+    // 消除数量
     eliminateNum += 2;
     setTimeout(function () {
         item1.addClass('flipOutY');
